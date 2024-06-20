06c6ae6f0ba8c46a7ffa77178fcd620de7d7cc8a
Jhonnee <joaobrenogs@gmail.com>
2024-06-19 22:39:42 -0300

    Criado 4 componentes e iniciado os estudos do angular

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from './componente01/componente01.component';
+import { Componente02Component } from "./componente02/componente02.component";
+import { Componente03Component } from "./componente03/componente03.component";
+import { Componente04Component } from "./componente04/componente04.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component]
 })
 export class AppComponent {
   title = 'angular-basico';
