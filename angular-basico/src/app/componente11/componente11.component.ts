8641a92fb008334cdd4bc7509f1ad559a11bca1e
João Breno <joaobrenogs@gmail.com>
2024-06-27 22:30:56 -0300

    Criado o componente11 e inciado a criação de um formulario usando bootstrap e ts

---
+++ angular-basico/src/app/componente11/componente11.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-componente11',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './componente11.component.html',
+  styleUrl: './componente11.component.css'
+})
+export class Componente11Component {
+
+  formulario = new FormGroup({
+    nome : new FormControl('', Validators.required),
+    idade : new FormControl(null),
+    cidade : new FormControl(''),
+  })
+
+}
