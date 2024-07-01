e1aa51303ee5673d579394b1a5b6104d810f5b2b
João Breno <joaobrenogs@gmail.com>
2024-07-01 19:11:21 -0300

    Finanlzado o Curso de Angular e partindo para inciar projetos utilizando angular na pratica

--- angular-basico/src/app/componente11/componente11.component.ts
+++ angular-basico/src/app/componente11/componente11.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Pessoa } from '../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
@@ -23,6 +24,8 @@ export class Componente11Component {
   //Pessoa
   vetor:Pessoa[] = [];
 
+  indice:number = -1
+
   //Função de cadastro
 
   cadastrar(){
@@ -37,6 +40,49 @@ export class Componente11Component {
     console.table(this.vetor);
 
   }
+  //Função Selecionar
+  Selecionar(indice:number){
 
+    //Atribuir o indice da pessoa
+    this.indice = indice
 
+    //Atribuir os dados da pessoa
+    this.formulario.setValue({
+      nome : this.vetor[indice].nome,
+      idade : this.vetor[indice].idade,
+      cidade : this.vetor[indice].cidade,
+
+    });
+
+    this.btnCadastrar = false;
+  }
+
+  //Função de alteração
+  Alterar(){  
+    this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+    //Limpas os inputs
+    this.formulario.reset()
+    
+    //Visibilidade dos botões
+    this.btnCadastrar = true;
+
+  }
+  //Função de exclusão
+  Remover(){
+    this.vetor.splice(this.indice, 1)
+
+    //Limpesa dos inputs
+    this.formulario.reset()
+
+    this.btnCadastrar = true
+  }
+  //Cancelar
+  Cancelar(){
+    //Limpar formulario
+    this.formulario.reset();
+    
+    //Visibilidade dos botões
+    this.btnCadastrar = true;
+  }
 }
